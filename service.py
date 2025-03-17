bf932b1019a9183e3f56250f2bde95fc669b38ed
Zhao Shenyang <dev@zsy.im>
2025-03-18 01:55:46 +0800

    feat: add synthsize endpoint

--- service.py
+++ service.py
@@ -14,6 +14,11 @@ from streaming_utils import StreamingInputs, predict_streaming_generator
 
 MODEL_ID = "coqui/XTTS-v2"
 
+SAMPLE_INPUT_DATA = {
+    'text': 'It took me quite a long time to develop a voice and now that I have it I am not going to be silent.',
+    'language': 'en',
+}
+
 runtime_image = bentoml.images.PythonImage(
     python_version="3.11",
     base_image="pytorch/pytorch:2.4.1-cuda12.4-cudnn9-devel",
@@ -49,11 +54,11 @@ class XTTSStreaming:
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        config = XttsConfig()
-        config.load_json(os.path.join(self.hf_model, "config.json"))
-        self.model = Xtts.init_from_config(config)
+        self.config = XttsConfig()
+        self.config.load_json(os.path.join(self.hf_model, "config.json"))
+        self.model = Xtts.init_from_config(self.config)
         self.model.load_checkpoint(
-            config,
+            self.config,
             checkpoint_dir=self.hf_model,
             eval=True,
             use_deepspeed=True if self.device == "cuda" else False
@@ -67,7 +72,31 @@ class XTTSStreaming:
         self.gpt_cond_latent = _t[0]
         self.speaker_embedding = _t[1]
 
-        
+    @bentoml.api
+    def synthesize(
+            self,
+            context: bentoml.Context,
+            text: str = SAMPLE_INPUT_DATA["text"],
+            lang: str = SAMPLE_INPUT_DATA["language"],
+    ) -> t.Annotated[Path, bentoml.validators.ContentType('audio/*')]:
+        import torch
+        import torchaudio
+
+        output_path = os.path.join(context.temp_dir, "output.wav")
+        sample_path = "./female.wav"
+        if not os.path.exists(sample_path):
+            sample_path = "./src/female.wav"
+
+        out = self.model.synthesize(
+            text,
+            self.config,
+            speaker_wav=sample_path,
+            language=lang,
+            enable_text_splitting=True,
+        )
+        torchaudio.save(output_path, torch.tensor(out["wav"]).unsqueeze(0), 24000)
+        return Path(output_path)
+
     @streaming_app.post("/stream")
     def tts_stream(self, inp: StreamingInputs):
         gen = predict_streaming_generator(
